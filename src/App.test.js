80fc76bf4878b82f274f4f1baaf55d33e5a4511f
Saulius Vikerta <svikerta@kayak.com>
2019-05-29 16:01:55 +0300

    adding testing suite setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App renders UI properly', () => {
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders main action button', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('button').exists()).toEqual(true);
+  })
 });
