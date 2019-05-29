12232dc40866c90c5d4929ed345ea35565bdc7dc
Saulius Vikerta <svikerta@kayak.com>
2019-05-29 16:37:31 +0300

    adding backend(api) tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import request from 'supertest';
 
 describe('App renders UI properly', () => {
 
@@ -12,4 +13,20 @@ describe('App renders UI properly', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('button').exists()).toEqual(true);
   })
+});
+
+describe('Backend API is responding', () => {
+
+  it('API request should respond with 200 status', async () => {
+    request(`localhost:3000`).get(`/api`)
+      .expect(200, {
+        statusCode: 200,
+      })
+  });
+
+  it('API responds with JSON content type', async () => {
+    request(`localhost:3000`).get(`/api`)
+      .expect('Content-Type', /json/);
+  });
+
 });
